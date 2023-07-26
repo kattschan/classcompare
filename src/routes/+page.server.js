cc1f9181202918a7f95ec471aaa87b9de6508f5d
Kat <kat@kattschan.co.uk>
2023-07-26 15:21:47 -0400

    removing id because it causes issues and we don't do anything with it

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -12,12 +12,12 @@ export const actions = {
 		const { classRows } =
 			await sql`SELECT * FROM classes WHERE class = ${usersclass} AND name != ${name}`;
 		console.log(classRows);
-		if (rows) return { name: name, class: usersclass, id: rows[0].id, classRows };
+		if (rows) return { name: name, class: usersclass, classRows };
 		// Add to database
 		const { addRows } = await sql`
                 INSERT INTO classes (name, class) VALUES (${name}, ${usersclass})
             `;
 		console.log(addRows);
-		return { name: name, class: usersclass, id: addRows[0].id, classRows };
+		return { name: name, class: usersclass, classRows };
 	}
 };
